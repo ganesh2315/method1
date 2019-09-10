34fcef83c114f7d69ac9c22019204bfd17b0d400
GANESH GHOSH <ganeshghosh02015@gmail.com>
2019-09-10 11:32:52 +0530

    method1

---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        myName("Ganesh");
+
+        int finalResult = addNums(4, 5);
+
+        System.out.println( finalResult);
+
+
+        System.out.println( fullName("Ganesh", "Ghosh"));
+
+        System.out.println(showChar('D'));
+
+
+    }
+
+    //Method
+    public static void myName(String mName) {
+
+        System.out.println(mName);
+
+    }
+
+    // Return an integer
+
+    public static int addNums(int a, int b) {
+        int result;
+        result = a + b;
+        //  System.out.println(" Sum = "  + (a + b));
+
+        return result ; // (a+b)
+    }
+
+    //Return a string
+
+    public static String fullName(String firstName, String lastName){
+
+        return firstName + " " + lastName;
+    }
+
+    // Return Character
+    public static char showChar(char c) {
+        return c;
+    }
+
+
+
+}
